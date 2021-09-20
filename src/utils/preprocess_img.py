1967dcd41ecfc20a9b46775a950735d9b32390a1
linyan <89194485+linyanAI@users.noreply.github.com>
2021-09-20 22:59:51 +0800

    Update the preprocess images by linyan

--- src/utils/preprocess_img.py
+++ src/utils/preprocess_img.py
@@ -1,16 +1,24 @@
+from posixpath import basename
 import cv2
 import os
 import numpy as np
 
+
 if __name__ == "__main__":
+    # preprocess the images and put the processed images to the new folder
     root = 'train'
     for Set in os.listdir(root):
-        print(Set)
+        # print(Set)
         if Set[:-1] == 'trainset':
             for user in os.listdir(os.path.join('train', Set)):
-                for pic in os.listdir(os.path.join('train/trainset1', '0000-0230')):
-                    print(pic)
-                    img = cv2.imread(os.path.join('train/trainset1/0000-0000', '0000-0000L_1000.jpg'))
-                    res = img[:500, 492:, :]
-                    cv2.imwrite('res.jpg', res)
-                    break
+                path = os.path.join(f'train/{Set}', user)
+                for pic in os.listdir(path):
+                    currpath = os.path.join(path, pic)
+                    dispath = 'pro_train/' + currpath[6:]
+                    print(os.path.abspath(dispath)[:-20])
+                    print(dispath)
+                    if not os.path.exists(os.path.abspath(dispath)[:-20]):
+                        os.makedirs(os.path.abspath(dispath)[:-20])
+                    img = cv2.imread(currpath)
+                    res = img[:500,492:,:]
+                    cv2.imwrite(dispath, res)
