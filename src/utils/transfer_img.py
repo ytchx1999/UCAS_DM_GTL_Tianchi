830f49f7363e7099a707adc17a12d6d51994e696
linyan <linyan_edu@163.com>
2021-10-07 20:05:32 +0800

    fixed bugs

--- src/utils/transfer_img.py
+++ src/utils/transfer_img.py
@@ -35,6 +35,7 @@ def transform_func(flag):
     return transform
 
 def read_img(path, flag, num):
+    print(path)
     transform = transform_func(flag)
     lb, la, rb, ra = [], [], [], []
     n1, n2, n3, n4 = 0, 0, 0, 0
